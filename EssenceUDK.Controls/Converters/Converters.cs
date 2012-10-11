97ec56c36df8d51758d30a6909b3a7c8854166ed
Xen <Xen@950191d2-321e-465b-a8de-7dfce684c78e>
2012-10-11 17:05:01 +0000

    addded Styles and a little revision of uodatamanager (from IEnumerable to IList)
    git-svn-id: svn://svn.code.sf.net/p/essence-udk/code/trunk@12 950191d2-321e-465b-a8de-7dfce684c78e

---
+++ EssenceUDK.Controls/Converters/Converters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+using EssenceUDK.Platform;
+
+namespace EssenceUDK.Controls.Converters
+{
+    public class ConverterImageSourceItemsLandsFromISourface : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var surface = value as ISurface;
+            if (surface != null)
+            {
+                var image = surface.Image.Clone();
+                return image;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+ }
