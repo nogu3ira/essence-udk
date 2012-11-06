4babd75da8dafd4609a9bc795abc92940c130e4b
Xen <Xen@950191d2-321e-465b-a8de-7dfce684c78e>
2012-11-06 15:31:26 +0000

    changed name to application project
    git-svn-id: svn://svn.code.sf.net/p/essence-udk/code/trunk@35 950191d2-321e-465b-a8de-7dfce684c78e

---
+++ UDK/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UDK.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
